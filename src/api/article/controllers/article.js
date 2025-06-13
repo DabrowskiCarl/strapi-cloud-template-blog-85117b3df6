459f2bb188fa666760606d6f4495db5d0a1c298d
dabrowski.carl <dabrowski.carl@gmail.com>
2025-06-13 11:00:51 +0000

    article.js change

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,29 @@
 'use strict';
-
 /**
  *  article controller
  */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) =>  ({
+  async find(ctx) {
+    // some custom logic here
+    ctx.query = { ...ctx.query, populate: 'deep' };
+    
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    // some more custom logic
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    ctx.query = { ...ctx.query, populate: 'deep' };
+    
+    // Calling the default core action
+    const { data, meta } = await super.findOne(ctx);
+
+    // some more custom logic
+    return { data, meta };
+  }
+}));
+
