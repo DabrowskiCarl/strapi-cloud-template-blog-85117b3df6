5d05d6856045e68c68d7f9231d310da9eecc4117
dabrowski.carl <dabrowski.carl@gmail.com>
2025-06-13 12:10:55 +0000

    changes slider

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -6,17 +6,15 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::article.article', ({ strapi }) =>  ({
   async find(ctx) {
-    // Poprawiona, bardziej precyzyjna logika populacji
+    // Bezpieczna, uproszczona logika do testowania
     ctx.query.populate = {
       cover: true, // Dołączamy obraz okładki
       blocks: {
-        // Używamy 'on', aby sprecyzować populację dla konkretnych komponentów
+        // Na razie próbujemy populować tylko komponent slidera
         on: {
-          'shared.slider': { populate: 'files' },
-          'shared.media': { populate: 'media' },
-          'shared.quote': true,
-          'shared.rich-text': true
-          // Upewnij się, że nazwy komponentów (np. 'shared.slider') są poprawne
+          'shared.slider': {
+            populate: 'files'
+          }
         }
       }
     };
@@ -26,15 +24,14 @@ module.exports = createCoreController('api::article.article', ({ strapi }) =>  (
   },
 
   async findOne(ctx) {
-    // Ta sama precyzyjna logika dla pojedynczego artykułu
+    // Ta sama bezpieczna logika dla pojedynczego artykułu
     ctx.query.populate = {
       cover: true,
       blocks: {
         on: {
-          'shared.slider': { populate: 'files' },
-          'shared.media': { populate: 'media' },
-          'shared.quote': true,
-          'shared.rich-text': true
+          'shared.slider': {
+            populate: 'files'
+          }
         }
       }
     };
